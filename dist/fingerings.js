5af923ae5b9691b8b48201710c2cc5fef1784e7b
fleidloff <frederik.leidloff@student.hpi.uni-potsdam.de>
2015-03-15 10:25:04 +0100

    improved example, added readme description and added full js file including raphael and teoria

--- dist/fingerings.js
+++ dist/fingerings.js
@@ -1,7 +1,7 @@
 // ///////////////////////////////////
 // fingerings.js
 //
-// build date: 2015-03-14
+// build date: 2015-03-15
 // version: 0.0.1
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
